d88149e4d3b67d60b4de763fabb2068019b374fb
zualmerul <135142064+zualmerul@users.noreply.github.com>
2024-08-05 12:04:32 +0200

    Update train_grok_chess.py

--- config/train_grok_chess.py
+++ config/train_grok_chess.py
@@ -13,7 +13,7 @@ wandb_run_name = "4layer_150masters_10decay"
 
 dataset = "lichess_hf_dataset"
 gradient_accumulation_steps = 1
-batch_size = 100
+batch_size = 256
 block_size = 1023  # context of up to 1023 tokens (because dataset block size is 1024)
 
 # baby GPT model :)
