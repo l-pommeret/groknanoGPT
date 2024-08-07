b93f2a60e95288e2c7d7844cb4abf241a3b251c7
l-pommeret <135142064+l-pommeret@users.noreply.github.com>
2024-08-07 11:52:54 +0200

    Update train_grok_tic_tac_toe.py

--- config/train_grok_tic_tac_toe.py
+++ config/train_grok_tic_tac_toe.py
@@ -1,9 +1,9 @@
 out_dir = "out-shakespeare-char"
-eval_interval = 50
-eval_iters = 50
+eval_interval = 200
+eval_iters = 200
 # I'm not sure what's going on, but when log_interval == 100, the time per iter is inaccurate and much longer than it should be
 # when running on multiple GPUs. TODO: investigate
-log_interval = 50  # don't print too too often
+log_interval = 200  # don't print too too often
 
 always_save_checkpoint = True
 
@@ -13,7 +13,7 @@ wandb_run_name = "2layer_26kgames_1decay_grokfast"
 
 dataset = "tic_tac_toe"
 gradient_accumulation_steps = 1
-batch_size = 512
+batch_size = 1024
 block_size = 26  # context of up to 1023 tokens (because dataset block size is 1024)
 
 # baby GPT model :)
@@ -23,7 +23,7 @@ n_embd = 128
 dropout = 0.0
 
 learning_rate = 0.00001
-max_iters = 10000000
+max_iters = 100000000
 lr_decay_iters = max_iters  # make equal to max_iters usually
 min_lr = 3e-5  # learning_rate / 10 usually
 beta2 = 0.95  # make a bit bigger because number of tokens per iter is small
